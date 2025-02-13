1046f26f44add7a3b9327232320e5d7f34204721
juniorcintra <junior.cintra.developer@gmail.com>
2025-02-13 17:01:24 -0300

    Refatorando JobController

--- src/main/java/br/com/juniorcintra/gestao_vagas/modules/company/controllers/JobController.java
+++ src/main/java/br/com/juniorcintra/gestao_vagas/modules/company/controllers/JobController.java
@@ -1,13 +1,16 @@
 package br.com.juniorcintra.gestao_vagas.modules.company.controllers;
 
+import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import br.com.juniorcintra.gestao_vagas.modules.company.dto.CreateJobDTO;
 import br.com.juniorcintra.gestao_vagas.modules.company.entities.JobEntity;
 import br.com.juniorcintra.gestao_vagas.modules.company.services.JobService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
 @RestController
@@ -18,9 +21,17 @@ public class JobController {
   private JobService jobService;
 
   @PostMapping()
-  public ResponseEntity<Object> create(@Valid @RequestBody JobEntity jobEntity) {
+  public ResponseEntity<Object> create(@Valid @RequestBody CreateJobDTO jobDTO,
+      HttpServletRequest request) {
 
     try {
+      var companyId = request.getAttribute("company_id");
+
+      var jobEntity = JobEntity.builder().title(jobDTO.getTitle())
+          .companyId(UUID.fromString(companyId.toString())).description(jobDTO.getDescription())
+          .benefits(jobDTO.getBenefits()).level(jobDTO.getLevel()).build();
+
+
       var result = this.jobService.create(jobEntity);
 
       return ResponseEntity.ok().body(result);
@@ -29,4 +40,5 @@ public class JobController {
     }
 
   }
+
 }
