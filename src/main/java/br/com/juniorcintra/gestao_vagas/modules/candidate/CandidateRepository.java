08369240681d83c8ecfd5595512ce6403f1f97a1
juniorcintra <junior.cintra.developer@gmail.com>
2025-02-12 23:42:22 -0300

    Criando Repositório

---
+++ src/main/java/br/com/juniorcintra/gestao_vagas/modules/candidate/CandidateRepository.java
@@ -0,0 +1,10 @@
+package br.com.juniorcintra.gestao_vagas.modules.candidate;
+
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CandidateRepository extends JpaRepository<CandidateEntity, UUID> {
+
+
+
+}
