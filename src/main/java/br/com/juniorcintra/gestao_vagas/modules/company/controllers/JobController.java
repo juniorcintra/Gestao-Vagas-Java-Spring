c1d9f5851674a79e20349698f4e6c5b9f4c21d14
juniorcintra <junior.cintra.developer@gmail.com>
2025-02-13 00:58:25 -0300

    Job Controller

---
+++ src/main/java/br/com/juniorcintra/gestao_vagas/modules/company/controllers/JobController.java
@@ -0,0 +1,32 @@
+package br.com.juniorcintra.gestao_vagas.modules.company.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import br.com.juniorcintra.gestao_vagas.modules.company.entities.JobEntity;
+import br.com.juniorcintra.gestao_vagas.modules.company.services.JobService;
+import jakarta.validation.Valid;
+
+@RestController
+@RequestMapping("/job")
+public class JobController {
+
+  @Autowired
+  private JobService jobService;
+
+  @PostMapping()
+  public ResponseEntity<Object> create(@Valid @RequestBody JobEntity jobEntity) {
+
+    try {
+      var result = this.jobService.create(jobEntity);
+
+      return ResponseEntity.ok().body(result);
+    } catch (Exception e) {
+      return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+  }
+}
---
+++ src/main/java/br/com/juniorcintra/gestao_vagas/modules/company/repositories/JobRepository.java
@@ -0,0 +1,8 @@
+package br.com.juniorcintra.gestao_vagas.modules.company.repositories;
+
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import br.com.juniorcintra.gestao_vagas.modules.company.entities.JobEntity;
+
+public interface JobRepository extends JpaRepository<JobEntity, UUID> {
+}
