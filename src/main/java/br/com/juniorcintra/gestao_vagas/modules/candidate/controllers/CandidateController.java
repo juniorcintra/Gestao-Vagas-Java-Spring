c3f931666c45240a0d85d0e74ae2afe901810ef0
juniorcintra <junior.cintra.developer@gmail.com>
2025-02-14 18:47:36 -0300

    Refatorando Documentação

--- src/main/java/br/com/juniorcintra/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/juniorcintra/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -23,12 +23,21 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/candidate")
+@Tag(name = "Candidato", description = "Informações do candidato")
 public class CandidateController {
 
   @Autowired
   private CandidateService candidateService;
 
   @PostMapping()
+  @Operation(summary = "Cadastro do candidato",
+      description = "Essa função é responsável cadastrar o candidato")
+  @ApiResponses({
+      @ApiResponse(responseCode = "200", description = "Informações do candidato cadastrado",
+          content = {
+              @Content(schema = @Schema(implementation = CandidateEntity.class))
+          }), @ApiResponse(responseCode = "400", description = "Candidato ja existe!")
+  })
   public ResponseEntity<Object> create(@Valid @RequestBody CandidateEntity candidateEntity) {
     try {
       var result = this.candidateService.create(candidateEntity);
@@ -42,7 +51,6 @@ public class CandidateController {
 
   @GetMapping()
   @PreAuthorize("hasRole('CANDIDATE')")
-  @Tag(name = "Candidato", description = "Informações do candidato")
   @Operation(summary = "Perfil do candidato",
       description = "Essa função é responsável por buscar as informações do perfil do candidato")
   @ApiResponses({
