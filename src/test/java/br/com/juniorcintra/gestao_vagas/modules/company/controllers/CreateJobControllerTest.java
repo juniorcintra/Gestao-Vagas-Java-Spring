8002f9e38abbb650ed6f562488cfbed5b6aeeeaf
juniorcintra <junior.cintra.developer@gmail.com>
2025-02-15 02:01:51 -0300

    JobController: Finalizando caso de teste com exceção

--- src/test/java/br/com/juniorcintra/gestao_vagas/modules/company/controllers/CreateJobControllerTest.java
+++ src/test/java/br/com/juniorcintra/gestao_vagas/modules/company/controllers/CreateJobControllerTest.java
@@ -1,6 +1,5 @@
 package br.com.juniorcintra.gestao_vagas.modules.company.controllers;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import java.util.UUID;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,7 +16,6 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import br.com.juniorcintra.gestao_vagas.exceptions.CompanyNotFoundException;
 import br.com.juniorcintra.gestao_vagas.modules.company.dto.CreateJobDTO;
 import br.com.juniorcintra.gestao_vagas.modules.company.entities.CompanyEntity;
 import br.com.juniorcintra.gestao_vagas.modules.company.repositories.CompanyRepository;
@@ -47,13 +45,10 @@ public class CreateJobControllerTest {
     var createJob = CreateJobDTO.builder().benefits("Todos").description("Teste").level("Pleno")
         .title("Desenvolvedor Java").build();
 
-    try {
-      mvc.perform(MockMvcRequestBuilders.post("/company/job")
-          .contentType(MediaType.APPLICATION_JSON).content(TestUtils.asJsonString(createJob))
-          .header("Authorization", TestUtils.generateToken(UUID.randomUUID(), "JAVAGAS_@123#")));
-    } catch (Exception e) {
-      assertThat(e).isInstanceOf(CompanyNotFoundException.class);
-    }
+    mvc.perform(MockMvcRequestBuilders.post("/company/job").contentType(MediaType.APPLICATION_JSON)
+        .content(TestUtils.asJsonString(createJob))
+        .header("Authorization", TestUtils.generateToken(UUID.randomUUID(), "JAVAGAS_@123#")))
+        .andExpect(MockMvcResultMatchers.status().isBadRequest());
   }
 
   @Test
